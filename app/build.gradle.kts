65fe53e4c9c2d9e86d1faf947826fca0f64d2d19
Muhammad <magdenbt@gmail.com>
2026-08-17 18:47:28 +0300

    upVersion 6 and fix release workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.vaynah.gochmott"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
