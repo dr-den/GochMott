a1c6b81c0cb7c5aa9f6f2af8685642c74138bef8
Muhammad <magdenbt@gmail.com>
2026-09-04 19:27:34 +0300

    chore: bump versionCode to 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.bilto.gochmott"
         minSdk = 26
         targetSdk = 37
-        versionCode = 15
+        versionCode = 16
         versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
